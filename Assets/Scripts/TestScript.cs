7070cb77546e50e0e3f116002c7b7c3df13f4f5a
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-02-20 15:53:59 +0100

    Added GameManager + Added TestScript + Added GameManager in scene

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    //Elements
+    [Header ("Elements")]
+    public Animator anim;
+    public Rigidbody2D rb;
+    [Tooltip ("This is a collider")]
+    public Collider2D col;
+
+
+    //Variable
+    [Header("Tweaks")]
+    [Range (0f,5f)]
+    public float speed;
+    [Range(0f, 5f)]
+    public float weight;
+
+    
+
+    
+    
+    void Awake()
+    {
+        
+    }
+    
+    void Start()
+    {
+   
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+    
+    
+}
