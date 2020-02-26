35aa58fa64387bfda41aacf88d5d8fc75eb4bebb
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-02-26 07:45:49 +0100

    Added AimAutoLock script + Hookables tag //STABLE

---
+++ Assets/Scripts/Player/AimAutoLock.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class AimAutoLock : MonoBehaviour
+{
+    //Elements 
+    [Header("Elements")]
+    public GameObject player;
+    private PlayerAim playerAim;
+
+
+    //Logic
+    private GameObject[] hookables;
+    private float distanceToHookable;
+    [HideInInspector] public bool locked;
+    private bool canLock = true;
+
+    [Header("Tweak")]
+    [Range(0f, 1f)]
+    public float autoLockDistance = 0.1f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");//Get the player
+        playerAim = player.GetComponent<PlayerAim>();
+
+        hookables = GameObject.FindGameObjectsWithTag("Hookable");//Get every hookable object in the scene
+        
+    }
+
+    void Update()
+    {
+        CheckHookables();
+
+        if(locked && playerAim.distanceToPlayer >= playerAim.maxRange)//If player is too far from locked crosshair, then it unlocks
+        {
+            StartCoroutine("CanLock");
+            locked = false;     
+        }
+
+        if(!locked)
+            gameObject.transform.SetParent(player.transform);//stick back to player
+    }
+
+    void CheckHookables()
+    {
+        for(int i = 0; i < hookables.Length; i++)
+        {
+            distanceToHookable = (gameObject.transform.position - hookables[i].transform.position).magnitude;
+
+            if(distanceToHookable <= autoLockDistance && !locked  && canLock)
+            {
+                Debug.Log(hookables.ToString());
+                canLock = false;
+                locked = true;
+                gameObject.transform.position = hookables[i].transform.position; //crosshair pos to target pos
+                gameObject.transform.SetParent(hookables[i].transform); //crossair sticks to target
+            }
+        }
+    }
+
+    public IEnumerator CanLock()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canLock = true;
+        yield return null;
+    }
+}
--- Assets/Scripts/Player/PlayerAim.cs
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -9,6 +9,7 @@ namespace Player
 
         [Header("Elements")]
         public GameObject crosshair;
+        private AimAutoLock aimAutoLock;
         [HideInInspector] public Transform crosshairTransform;
         private Transform playerTransform;
 
@@ -19,9 +20,10 @@ namespace Player
         private float horizontalAim;
         private float verticalAim;
         private Vector3 direction;
-        private float distanceToPlayer;
+        [HideInInspector] public float distanceToPlayer;
 
         private bool coroutineCanStart = false;
+        private bool unlockCoroutineCanStart = true;
 
         [Header("Tweak")]
         [Range(1f, 5f)]
@@ -30,6 +32,7 @@ namespace Player
         void Start()
         {
             crosshairTransform = crosshair.transform;
+            aimAutoLock = crosshair.GetComponent<AimAutoLock>();
             playerTransform = gameObject.transform;
         }
 
@@ -54,18 +57,24 @@ namespace Player
                 direction = new Vector3(horizontalAim, verticalAim, 0).normalized;
                 //on ralentit le joueur
 
-                if (distanceToPlayer <= maxRange) //On peut bouger le viseur jusqu'a maxRange.
+                if (distanceToPlayer <= maxRange && !aimAutoLock.locked) //On peut bouger le viseur jusqu'a maxRange.
                 {
                     crosshairTransform.position += direction * Time.deltaTime * speed;//on déplace le viseur                                                              
+                }
+
+                if (aimAutoLock.locked)
+                {
+                    if(unlockCoroutineCanStart)
+                        StartCoroutine("Unlock");
                 }
             }
 
             else if (horizontalAim == 0 && verticalAim == 0)//Si le joystick est à 0,0 alors la visée est désactivée après 1 sec.
-            {    
-                if(coroutineCanStart)
+            {
+                if (coroutineCanStart && !aimAutoLock.locked)
                 {
                     StartCoroutine("HideCrosshair");
-                }                
+                }
             }
         }
 
@@ -73,11 +82,26 @@ namespace Player
         {
             coroutineCanStart = false;
             yield return new WaitForSeconds(1f);
-            if (horizontalAim == 0 && verticalAim == 0)
+            if (horizontalAim == 0 && verticalAim == 0 && !aimAutoLock.locked)
             {
                 crosshair.SetActive(false);
                 //reset la vitesse du joueur
             }
+            yield return null;
+        }
+
+        private IEnumerator Unlock()//Sil le joueur utilise le joystick pendant plus de 0.3 sec alors que le viseur est lock, ce dernier se unlock.
+        {
+            unlockCoroutineCanStart = false;
+            yield return new WaitForSeconds(0.3f);
+
+            if(horizontalAim != 0 || verticalAim != 0 && aimAutoLock.locked)
+            {
+                aimAutoLock.locked = false;
+                aimAutoLock.StartCoroutine("CanLock");
+            }
+
+            unlockCoroutineCanStart = true;
             yield return null;
         }
     }
