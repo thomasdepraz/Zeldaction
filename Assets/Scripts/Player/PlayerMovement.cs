add5908de5f92e825bf2ee7b11da4df2b2113afd
Timothé Huerre <t.huerre@rubika-edu.com>
2020-02-26 16:27:41 +0100

    Completion of PlayerMovement (on TimScene) //STABLE
    Player prefab is NOT up to date with the PlayerMovement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+  //Player 
+    [Header("Player variables")]
+    public Rigidbody2D playerRb;
+    public Vector3 playerControl;
+    public float playerSpeed = 500;
+    private float controllerDeadzone = 0.25f;
+
+  //Player inputs
+    [Header("Player inputs")]
+    [Range(-1f, 1f)]
+    public float playerBindHorizontal;
+    [Range(-1f, 1f)]
+    public float playerBindVertical;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,33 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetPlayerController();
     }
+
+    // Fixed update
+    void FixedUpdate()
+    {
+        Movement();
+	}
+
+    void Movement()
+    {
+        playerControl = new Vector3(playerBindHorizontal, playerBindVertical);
+        if (playerControl.magnitude < controllerDeadzone)
+        {
+            playerControl = Vector2.zero;
+            playerRb.velocity = Vector2.zero;
+        }
+        else
+        {
+            playerRb.velocity = new Vector2(playerBindHorizontal * playerSpeed * Time.deltaTime, playerBindVertical * playerSpeed * Time.deltaTime);
+        }    
+
+	}
+    void GetPlayerController()
+    {
+        playerBindHorizontal = Input.GetAxis("Horizontal");
+        playerBindVertical = Input.GetAxis("Vertical");
+    }
+
 }
