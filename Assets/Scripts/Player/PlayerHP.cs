917cc5b858551fe3fdac9e73ce157b2a4644970e
Timothé HUERRE <t.huerre@rubika-edu.com>
2020-03-17 14:50:19 +0100

    Added "Heal" function on PlayerHP, changed an input in InputManager

--- Assets/Scripts/Player/PlayerHP.cs
+++ Assets/Scripts/Player/PlayerHP.cs
@@ -6,6 +6,7 @@ public class PlayerHP : MonoBehaviour
     [Range (1,20)]
     public int maxHealth = 10;
     public int currentHealth;
+    public int heal = 2;
 
     public HealthBar healthBar;
     // Start is called before the first frame update
@@ -15,6 +16,10 @@ public class PlayerHP : MonoBehaviour
         healthBar.SetMaxHealth(maxHealth); // set la barre de vie avec la vie maximale du joueur
     }
 
+    void Update()
+    {
+        GainLife(heal);
+    }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage; // le montant des dommages va être soustrait à la vie actuelle du joueur
@@ -31,5 +36,14 @@ public class PlayerHP : MonoBehaviour
         GetComponent<PlayerMovement>().enabled = false;
         GetComponent<PlayerAttack>().enabled = false;
         this.enabled = false;
+    }
+    void GainLife( int heal)
+    {
+        if (Input.GetButtonDown("DebugHeal"))
+        {
+            currentHealth += heal;
+            Debug.Log("pharmacy");
+            healthBar.SetHealth(currentHealth);
+        }
     }
 }
