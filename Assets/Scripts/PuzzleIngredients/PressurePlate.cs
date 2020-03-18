02fe557288ee426df61cd70f87af7ce8975cd5e4
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-03-18 15:32:41 +0100

    Added Pressure plate script + prefab

---
+++ Assets/Scripts/PuzzleIngredients/PressurePlate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Sprite realesedSprite;
+    public Sprite pressedSprite;
+    private SpriteRenderer spr;
+
+    public bool isPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isPressed = true;
+        spr.sprite = pressedSprite;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isPressed = false;
+        spr.sprite = realesedSprite;
+    }
+}
