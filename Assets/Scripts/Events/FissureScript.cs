740eb94ffee5692fd246f3ecde290d421b9b2d1a
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-03-25 19:32:38 +0100

    Update Level Design (Fissure + FissureScript + Add Environnement)

---
+++ Assets/Scripts/Events/FissureScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FissureScript : MonoBehaviour
+{
+    public GameObject fissure;
+    public GameObject redEyes;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        fissure.SetActive(false);
+        redEyes.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        fissure.SetActive(true);
+        redEyes.SetActive(false);
+    }
+}
