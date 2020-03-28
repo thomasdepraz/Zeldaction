feba9741afe2bbf1d1bcb04ebdf57bee4cf7a74b
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-03-28 18:07:24 +0100

    Scripting Phish + Narrative Events (meeting fish & Speaking with PNJ)

---
+++ Assets/Scripts/PNJ/Phish/PhishMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhishMovement : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    private bool canMove = true;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+        
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > stoppingDistance && canMove == true)
+        {
+            if (gameObject.activeInHierarchy)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector2.zero;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PNJ/Phish/PhishSpawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhishSpawn : MonoBehaviour
+{
+    public GameObject phish;
+    private GameObject player;
+    private PlayerMovement movement;
+
+    //Dialog
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool dialogActive;
+
+    //Feedback
+    public GameObject button;
+    public GameObject interrogationPoint;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        movement = player.GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (dialogBox.activeInHierarchy && Input.GetButtonDown("interact"))
+        {
+            dialogBox.SetActive(false);
+            movement.canMove = true;
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interrogationPoint.SetActive(true);
+            movement.canMove = false;
+            StartCoroutine(PhishSpeak());            
+        }
+    }
+
+    IEnumerator PhishSpeak()
+    {
+        yield return new WaitForSeconds(2f);
+        interrogationPoint.SetActive(false);
+        phish.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        dialogBox.SetActive(true);
+        dialogText.text = dialog;
+        button.SetActive(true);
+    }
+}
