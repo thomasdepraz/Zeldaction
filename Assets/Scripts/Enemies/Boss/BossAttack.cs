4305c8e54ce6bff6aa9005324a5faa65e89237b8
Timothé HUERRE <t.huerre@rubika-edu.com>
2020-03-30 11:08:54 +0200

    Added Idle Animation, Working on Animator & State Machines

--- Assets/Scripts/Enemies/Boss/BossAttack.cs
+++ Assets/Scripts/Enemies/Boss/BossAttack.cs
@@ -47,12 +47,13 @@ public class BossAttack : MonoBehaviour
     public GameObject milieu;
 
 
+
     // Update is called once per frame
     void Update()
     {
         if (playerIsDetected == true /*&& coroutineCanStart == true*/)
         {
-            StartCoroutine(Claw());
+            //StartCoroutine(Claw());
         }
     }
     private void FixedUpdate()
@@ -67,34 +68,33 @@ public class BossAttack : MonoBehaviour
     {
         playerIsDetected = false;
     }
-
-    IEnumerator Claw()
-    {
-        coroutineCanStart = false;
-        float destination = Mathf.PI;
-        if (angle > -destination)
-        {
-            posX = transform.position.x + Mathf.Cos(angle) * rotationRadius;
-            posY = transform.position.y + Mathf.Sin(angle) * rotationRadius;
-            claw.transform.position = new Vector2(posX, posY);
-            //claw.transform.position = Vector2.Lerp(claw.transform.position, milieu.transform.position );
-            angle = angle - Time.deltaTime * angularSpeed;
-            if (Mathf.Abs(angle) >= 2 * Mathf.PI)
-                angle = 0f;
-            yield return new WaitForSeconds(prepairTime);
-
-            Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(claw.transform.position, attackRange);
-
-
-            //claw.transform.localEulerAngles = new Vector3(0.0f, 0.0f, detectAngle);
-            //float chargeTime = 0f;
-            yield return new WaitForSeconds(stunTime);
-            coroutineCanStart = true;
-        }
-    }
-
 }
 /* Quand je rentre dans la zone de collision du boss, il se dirige vers moi puis, quand je rentre dans sa zone d'aggression, donne un coup de pince à la position que j'ai au moment ou il
  décide d'attaquer.
  Son coup de pince prends la forme d'un collider sur un gameobject enfant "pince", qui va effectuer un quart de rotation autour du boss.
  1ere solution envisagée: circle collider, quand j'entre dedans, le bosse s'arrete et donne un coup qui va tourner autour de ce collider*/
+
+/*IEnumerator Claw()
+{
+    coroutineCanStart = false;
+    float destination = Mathf.PI;
+    if (angle > -destination)
+    {
+        posX = transform.position.x + Mathf.Cos(angle) * rotationRadius;
+        posY = transform.position.y + Mathf.Sin(angle) * rotationRadius;
+        claw.transform.position = new Vector2(posX, posY);
+        //claw.transform.position = Vector2.Lerp(claw.transform.position, milieu.transform.position );
+        angle = angle - Time.deltaTime * angularSpeed;
+        if (Mathf.Abs(angle) >= 2 * Mathf.PI)
+            angle = 0f;
+        yield return new WaitForSeconds(prepairTime);
+
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(claw.transform.position, attackRange);
+
+
+        //claw.transform.localEulerAngles = new Vector3(0.0f, 0.0f, detectAngle);
+        //float chargeTime = 0f;
+        yield return new WaitForSeconds(stunTime);
+        coroutineCanStart = true;
+    }*/
+
