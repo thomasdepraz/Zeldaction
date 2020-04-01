382a3c06ab9c7434e662c3adda703695ed68aa59
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-04-01 16:01:17 +0200

    Added RigidPressurePlate

--- Assets/Scripts/PuzzleIngredients/PressurePlate.cs
+++ Assets/Scripts/PuzzleIngredients/PressurePlate.cs
@@ -7,23 +7,36 @@ public class PressurePlate : MonoBehaviour
     public Sprite realesedSprite;
     public Sprite pressedSprite;
     public LayerMask hitboxLayer;
-    public bool isRigid;
     private SpriteRenderer spr;
     private BoxCollider2D col;
 
 
     public bool isPressed;
+    public bool isRigid;
+    public int objectsNeeded;
+    private int objectsOnPlate = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
         col = gameObject.GetComponent<BoxCollider2D>();
         spr = gameObject.GetComponent<SpriteRenderer>();
     }
+
     private void Update()
     {
+        if(objectsOnPlate >= objectsNeeded && isRigid)
+        {
+            isPressed = true;
+            spr.sprite = pressedSprite;
+        }
 
+        else if(objectsOnPlate < objectsNeeded && isRigid)
+        {
+            isPressed = false;
+            spr.sprite = realesedSprite;
+        }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     { 
         if(collision.gameObject.layer == 10 && !isRigid)
@@ -31,18 +44,28 @@ public class PressurePlate : MonoBehaviour
             isPressed = true;
             spr.sprite = pressedSprite;
         }
-        
 
-        
+
+        if(collision.TryGetComponent(out Crate crate) && isRigid)
+        {
+            if(crate.isHeavyCrate)
+                objectsOnPlate++;
+        }
+ 
     }
+
     private void OnTriggerExit2D(Collider2D collision)
-    {
-        
-        Debug.Log(col.IsTouchingLayers(hitboxLayer));
-        if(!col.IsTouchingLayers(hitboxLayer))
+    { 
+        if(!col.IsTouchingLayers(hitboxLayer) && !isRigid)
         {
             isPressed = false;
             spr.sprite = realesedSprite;
-        } 
+        }
+
+        if (collision.gameObject.TryGetComponent(out Crate crate) && isRigid)
+        {
+            if (crate.isHeavyCrate)
+                objectsOnPlate--;
+        }           
     }
 }
