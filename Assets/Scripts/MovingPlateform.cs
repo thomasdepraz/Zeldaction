233ec66fef140cb0c10c4e3ae71a165fe596fd56
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-04-10 19:06:25 +0200

    Added MovingPlateform Script (Plateform can move have move points)

---
+++ Assets/Scripts/MovingPlateform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlateform : MonoBehaviour
+{
+    public float speed;
+    public Transform pos1, pos2;
+    public Transform startPos;
+
+    Vector3 nextPos;
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    void Update()
+    {       
+        if(transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        if(transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
+    }
+
+    // Le PJ est parenté par les déplacements de la plateform lorsqu'il se trouve dessus. 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
