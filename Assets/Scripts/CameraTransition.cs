6eb0ec7340914eb6c2a204e8b2bd6cd4d28541c1
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-04-23 18:33:01 +0200

    Start Event Systeme (interpellation Script + Update Dialog)

---
+++ Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public CinemachineVirtualCamera playerCam;
+    public CinemachineVirtualCamera transitionCam;
+
+
+    // Transition de caméra 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerCam.gameObject.SetActive(false);
+        transitionCam.gameObject.SetActive(true);
+    }
+}
