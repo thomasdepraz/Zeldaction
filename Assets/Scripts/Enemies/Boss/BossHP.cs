0c3c2191af6e0f470eed5dd86829bc6b9f112c23
Timothé HUERRE <t.huerre@rubika-edu.com>
2020-04-24 17:13:35 +0200

    Phase 2 "finished"

---
+++ Assets/Scripts/Enemies/Boss/BossHP.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHP : MonoBehaviour
+{
+    [Range(0, 20)]
+    public int maxHealth = 10;
+    public int currentHealth;
+    bool isPhase1;
+    //bool isPhase2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        isPhase1 = GetComponent<BossManager>().isPhase1;
+        //isPhase2 = GetComponent<BossManager>().isPhase2;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage; // le montant des dommages va être soustrait à la vie actuelle de l'ennemi
+        StartCoroutine(DamageFB());
+        if (currentHealth <= 0 && isPhase1 == true)
+        {
+            gameObject.GetComponent<BossManager>().Phase2();
+            currentHealth = maxHealth;
+        }
+        /*  else if (currentHealth <= 0 && isPhase2 == true)
+          {
+
+          }*/
+    }
+    IEnumerator DamageFB()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
---
+++ Assets/Scripts/Enemies/Boss/BossManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    public Animator anim;
+    public bool isPhase1 = true;
+    public bool isPhase2 = false;
+    public bool isPhase3 = false;
+    public GameObject plate1;
+    public GameObject plate2;
+    private GameObject Plate1;
+    private GameObject Plate2;
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+    public GameObject stone;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPhase2 == true)
+        {
+            if (Plate1.GetComponent<PressurePlate>().isPressed && Plate2.GetComponent<PressurePlate>().isPressed)
+            {
+                Phase3();
+            }
+        }
+    }
+    public void Phase2()
+    {
+        anim.SetTrigger("GoPhase2"); // intégrer l'animation de déplacement du Boss vers sa plateforme
+        isPhase1 = false;
+        isPhase2 = true;
+        gameObject.GetComponent<BossMovement>().enabled = false;
+        gameObject.GetComponent<BossAttack>().enabled = false;
+        Plate1 = Instantiate(plate1, waypoint1.transform.position, Quaternion.identity);
+        Plate2 = Instantiate(plate2, waypoint2.transform.position, Quaternion.identity);
+    }
+    public void Phase3()
+    {
+        stone.SetActive(true);
+        anim.SetTrigger("GoPhase3"); // intégrer l'animation de transition de phase
+        isPhase3 = true;
+        isPhase2 = false;
+    }
+}
