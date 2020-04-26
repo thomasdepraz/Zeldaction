0c8109e3b00dfe246998d34dabf32628774f646e
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-04-26 17:08:21 +0200

    Added le truc chiant de mathieu + optimisation compilig unity

--- Assets/Scripts/Events/CombatEvent.cs
+++ Assets/Scripts/Events/CombatEvent.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CombatEvent : MonoBehaviour
 {
@@ -12,8 +10,9 @@ public class CombatEvent : MonoBehaviour
     private BoxCollider2D fightTrigger;
     public GameObject arenaCollider;
 
-    [Header("Objects to activate")]
+    [Header("Objects to activate / deactivate")]
     public GameObject[] objects;
+    public GameObject[] deactivateObjects;
 
     // Start is called before the first frame update
     void Start()
@@ -50,6 +49,13 @@ public class CombatEvent : MonoBehaviour
                 for (int i = 0; i < objects.Length; i++)
                 {
                     objects[i].SetActive(true);
+                }
+            }
+            if (deactivateObjects != null)
+            {
+                for (int i = 0; i < objects.Length; i++)
+                {
+                    deactivateObjects[i].SetActive(false);
                 }
             }
         }
