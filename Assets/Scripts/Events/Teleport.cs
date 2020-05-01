fb0ce3f046775f1533e56d81b880c558e0ba0443
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-05-01 13:11:34 +0200

    Teleport script +  activeParticle script + Lit Torch prefabs

---
+++ Assets/Scripts/Events/Teleport.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    private GameObject player;
+    public Transform targetPosition;
+    public GameObject transitionPanel;
+    private Animator anim;
+    [HideInInspector] public bool teleport;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = transitionPanel.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(teleport)
+        {
+            transitionPanel.SetActive(true);
+            anim.SetBool("teleport", true);
+        }
+    }
+
+    public void Teleportation()
+    {
+        player.transform.position = targetPosition.position;
+        anim.SetBool("teleport", false);
+        teleport = false;
+    }
+}
