6b8baba1e1def043862cfd674b29cf8ca1f62a9f
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-05-04 22:02:22 +0200

    Recuperate Hook

---
+++ Assets/Scripts/RecuperateHook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecuperateHook : MonoBehaviour
+{
+    bool inRange = false;
+    public GameObject button;
+      
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange == true && Input.GetButtonDown("interact"))
+        {
+            PlayerManager.hasHook = true;
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        inRange = true;
+        button.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inRange = false;
+        button.SetActive(false);
+    }
+}
