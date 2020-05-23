c07e4d58b965b8e48c1a3946bb3951e2b00edec2
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-05-23 12:21:01 +0200

    Update dialog grey layer +blur

---
+++ Assets/Scripts/UI/DialogUIManager.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DialogUIManager : MonoBehaviour
+{
+    public Volume postProcessingManager;
+    public VolumeProfile defaultProfile;
+    public VolumeProfile dialogProfile;
+  
+  
+
+    private void OnEnable()
+    {
+        postProcessingManager.profile = dialogProfile;
+    }
+    private void OnDisable()
+    {
+        postProcessingManager.profile = defaultProfile;
+    }
+}
