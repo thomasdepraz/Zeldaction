bcddc74f742754f9a9a869108c2af98d8bde309c
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-05-25 10:56:10 +0200

    Added VolumeManager

---
+++ Assets/Scripts/UI/VolumeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeManager : MonoBehaviour
+{
+    //Master Mixers
+    public AudioMixer player;
+    public AudioMixer props;
+    public AudioMixer UI;
+    public AudioMixer enemy;
+    public AudioMixer boss;
+
+    public void setVolume(float volume)
+    {
+        //volume *= 40;
+        player.SetFloat("MyExposedParam", volume);
+        props.SetFloat("MyExposedParam", volume);
+        UI.SetFloat("MyExposedParam", volume);
+        enemy.SetFloat("MyExposedParam", volume);
+        boss.SetFloat("MyExposedParam", volume);
+    }
+}
